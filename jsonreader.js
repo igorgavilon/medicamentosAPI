890f096cc281e3fa750f3bbae096b9117d804f17
Igor <igorgavilon@gmail.com>
2020-11-29 13:57:05 -0400

    novo commit

--- jsonreader.js
+++ jsonreader.js
@@ -3,17 +3,16 @@ function loadDrugs(){
     var url_string = window.location.href;
     var url = new URL(url_string);
     var searchTerm = url.searchParams.get("search");
-    var saida = "";
+    var saida = [];
 
     if(searchTerm != null){
         if(searchTerm != ""){
             $.getJSON("medicamentos.json", function(data) {
                 medicamentos = data;
-                var saida= '';
                 saida += searchTerm;
                 saida += "</BR>";
                 for(i=0; i < 2; i++){
-                    saida += medicamentos[i];
+                    saida += JSON.parse(medicamentos[i]);
                     saida += "</BR>";
                 }
                 alert("Entrou no getJSON");
