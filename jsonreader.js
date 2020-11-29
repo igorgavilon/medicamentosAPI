a743a0a5d26e8257510f8fd0001921be41ec5872
Igor <igorgavilon@gmail.com>
2020-11-29 13:13:52 -0400

    novo commit

--- jsonreader.js
+++ jsonreader.js
@@ -3,18 +3,27 @@ function loadDrugs(){
     var url_string = window.location.href;
     var url = new URL(url_string);
     var searchTerm = url.searchParams.get("search");
-    
-    $.getJSON("medicamentos.json", function(data) {
-        medicamentos = data;
-        var saida= '';
-        saida += searchTerm;
-        saida += "</BR>";
-        for(i=0; i < 2; i++){
-            console.log(medicamentos[i].PRODUTO);
-            saida += medicamentos[i].PRODUTO;
-            saida += "</BR>";
+    var saida = "";
+
+    if(searchTerm != null){
+        if(searchTerm != ""){
+            $.getJSON("medicamentos.json", function(data) {
+                medicamentos = data;
+                var saida= '';
+                saida += searchTerm;
+                saida += "</BR>";
+                for(i=0; i < 2; i++){
+                    saida += medicamentos[i];
+                    saida += "</BR>";
+                }
+                alert("Entrou no getJSON");
+                document.getElementById('tela').innerHTML = saida;
+            });
+        }else{
+            saida = "termo da pesquisa não informado (em branco)!";
         }
-        alert("Entrou no getJSON");
-        document.getElementById('tela').innerHTML = saida;
-    });
+    }else{
+        saida = "termo da pesquisa não informado (null)!";
+    }
+
 }
