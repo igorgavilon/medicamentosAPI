74789aefb57a89425df0b7130fa9017f9cfd8e02
Igor <igorgavilon@gmail.com>
2020-11-29 12:00:15 -0400

    criacao dos arquivos da API

---
+++ jsonreader.js
@@ -0,0 +1,19 @@
+function loadDrugs(){
+    alert("chamou a função");
+    /*
+    $.getJSON("medicamentos.json", function(data) {
+        medicamentos = data;
+        var saida= '';
+
+        for(i=0; i < 2; i++){
+            console.log(medicamentos[i].PRODUTO);
+            saida += medicamentos[i].PRODUTO;
+        }
+        document.getElementById('tela').innerHTML = 'funcionou';
+    });
+    */
+
+    var dados = JSON.parse(medicamentos);
+    alert("hello");
+    document.getElementById('tela').innerHTML = dados[0].PRODUTO;
+}
