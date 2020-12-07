40426a19b8e6d9e34ac32566bba3f10b2721574e
Igor Gavilon <igorgavilon@gmail.com>
2020-12-07 12:09:18 -0400

    Update jsonreader.js

--- jsonreader.js
+++ jsonreader.js
@@ -11,7 +11,7 @@ function loadDrugs(){
                 medicamentos = data;
                 saida += searchTerm;
                 //saida += "</BR>";
-                saida += (medicamentos[0].PRODUTO);
+                saida += (medicamentos[0]);
                 //for(i=0; i < 2; i++){
                  //   saida += (medicamentos[i].PRODUTO);
                  //   saida += "</BR>";
